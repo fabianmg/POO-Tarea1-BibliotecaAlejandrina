35dae202fe0d7d8d40dbf6630a3d6b1463782035
arielmmonestel <arielmmonestel@gmail.com>
2014-09-04 21:57:35 -0600

    Avance de Intefaz grafica

--- src/acceso/AccesoBiblioteca.java
+++ src/acceso/AccesoBiblioteca.java
@@ -8,6 +8,14 @@ import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JDesktopPane;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JInternalFrame;
 
 public class AccesoBiblioteca extends JFrame {
 
@@ -19,7 +27,7 @@ public class AccesoBiblioteca extends JFrame {
 	 */
 	public static void main(String[] args) throws IOException {
 		
-		///code temp////////////
+		///code temnp////////////
 			//  public void GuardarPersona(String nombre, String apellido1, String apellido2, String telefono, String email,String  tipo ) throws IOException
 		Biblioteca personas = new Biblioteca();
 		
@@ -46,11 +54,47 @@ public class AccesoBiblioteca extends JFrame {
 	 */
 	public AccesoBiblioteca() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 600, 441);
+		
+		JMenuBar menuPrincipal = new JMenuBar();
+		setJMenuBar(menuPrincipal);
+		
+		JMenu menuAgregar = new JMenu("Agregar");
+		menuPrincipal.add(menuAgregar);
+		
+		JMenuItem agregarPersona = new JMenuItem("Persona");
+		agregarPersona.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+								
+				
+			}
+		});
+		menuAgregar.add(agregarPersona);
+		
+		JMenuItem agregarLibro = new JMenuItem("Libro");
+		menuAgregar.add(agregarLibro);
+		
+		JMenu menuConsultar = new JMenu("Consultar");
+		menuPrincipal.add(menuConsultar);
+		
+		JMenuItem mntmPorPersona = new JMenuItem("Por Persona");
+		menuConsultar.add(mntmPorPersona);
+		
+		JMenuItem mntmPorLibro = new JMenuItem("Por Libro");
+		menuConsultar.add(mntmPorLibro);
+		
+		JMenu menuParametros = new JMenu("Parametros");
+		menuPrincipal.add(menuParametros);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JDesktopPane escritorio = new JDesktopPane();
+		escritorio.setBounds(0, 0, 584, 381);
+		contentPane.add(escritorio);
+		
+		JInternalFrame ventanaAgregarNombres = new JInternalFrame("Agregar Nombres");
+		ventanaAgregarNombres.setBounds(0, 0, 584, 381);
 	}
-
 }
