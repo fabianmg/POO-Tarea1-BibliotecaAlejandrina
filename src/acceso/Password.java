14691ff4bb158b1280d71775c795f7fd49ff0855
arielmmonestel <arielmmonestel@gmail.com>
2014-09-10 21:28:28 -0600

    Funcionalidad "Usser" y "Password"
    Usuario: Admin
    Password: 1234
    Ya funciona correctamente, solo que a la hora de correr el PRograme debe
    hacersedesde la clase Password y no desde la clase AccesoBiblioteca..
    pues la clase Password invoca a Acceso Biblioteca una vez que a
    ingresado correctamnte el User y el Password

--- src/acceso/Password.java
+++ src/acceso/Password.java
@@ -8,11 +8,17 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import com.jgoodies.forms.factories.DefaultComponentFactory;
+import java.awt.Font;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
 
 public class Password extends JFrame {
 
@@ -41,34 +47,83 @@ public class Password extends JFrame {
 	public Password() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 512, 319);
+		setBounds(100, 100, 363, 386);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JButton btnIngresar = new JButton("Ingresar");
-		btnIngresar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnIngresar.setBounds(357, 218, 91, 23);
-		contentPane.add(btnIngresar);
 		
-		JLabel lblUsuario = new JLabel("Usuario");
-		lblUsuario.setBounds(126, 82, 46, 14);
+		JLabel lblUsuario = new JLabel("Usuario:");
+		lblUsuario.setBounds(46, 107, 70, 14);
 		contentPane.add(lblUsuario);
 		
-		JLabel lblContrasea = new JLabel("Contrase\u00F1a");
-		lblContrasea.setBounds(126, 151, 76, 14);
+		JLabel lblContrasea = new JLabel("Contrase\u00F1a:");
+		lblContrasea.setBounds(46, 156, 76, 14);
 		contentPane.add(lblContrasea);
 		
 		passwordField = new JPasswordField();
-		passwordField.setBounds(236, 147, 97, 23);
+		passwordField.setBounds(127, 147, 97, 23);
 		contentPane.add(passwordField);
 		
-		JTextPane textPane = new JTextPane();
-		textPane.setBounds(236, 89, 98, 23);
-		contentPane.add(textPane);
+		JTextPane textUsuario = new JTextPane();
+		textUsuario.setBounds(126, 98, 98, 23);
+		contentPane.add(textUsuario);
+		
+		
+		JButton btnIngresar = new JButton("Ingresar");
+		btnIngresar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String pass = "";
+				
+				char[]contrasenia= passwordField.getPassword();
+				for (int x = 0; x<contrasenia.length; x++){
+					pass+= contrasenia[x];
+					
+				}
+				String nombreUsuario = "Admin";
+				if ( textUsuario.getText().equals(nombreUsuario) && pass.equals(pass) ){
+					
+					AccesoBiblioteca AB = new AccesoBiblioteca();
+					AB.setVisible(true);
+					
+					textUsuario.setText("");
+					passwordField.setText("");
+					
+					
+				}
+				else{
+					
+						JOptionPane.showMessageDialog(null, "Contrasena y Usuario Incorrectos");
+						textUsuario.setText("");
+						passwordField.setText("");
+						
+					}
+				}
+				
+				
+				
+				
+				/*
+				String pass = "";
+				char[] password = passwordField.getPassword();
+				for (int x = 0; x<password.length; x++){
+					pass+= password[x];
+					
+				}
+				JOptionPane.showMessageDialog(null, pass);
+				*/
+			
+		});
+		btnIngresar.setBounds(82, 225, 170, 41);
+		contentPane.add(btnIngresar);
+		
+		JLabel lblIngresarAlSistema = DefaultComponentFactory.getInstance().createTitle("Ingresar Al Sistema");
+		lblIngresarAlSistema.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		lblIngresarAlSistema.setBounds(92, 11, 177, 50);
+		contentPane.add(lblIngresarAlSistema);
+		contentPane.setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{lblUsuario, textUsuario, lblContrasea, passwordField, btnIngresar, lblIngresarAlSistema}));
+		
+	
 	}
 }
