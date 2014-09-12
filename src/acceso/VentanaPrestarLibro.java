655dd4f96c60ca32ab1d8245fef0790217fc9029
arielmmonestel <arielmmonestel@gmail.com>
2014-09-11 21:12:55 -0600

    Enviar Emails a personas Morosas. OK
    ...

--- src/acceso/VentanaPrestarLibro.java
+++ src/acceso/VentanaPrestarLibro.java
@@ -14,10 +14,14 @@ import java.util.ArrayList;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JLabel;
+
 import com.jgoodies.forms.factories.DefaultComponentFactory;
+
 import javax.swing.JTextPane;
+
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.SystemColor;
@@ -49,10 +53,6 @@ public class VentanaPrestarLibro extends JInternalFrame {
 		setBounds(10, 11, 774, 398);
 		getContentPane().setLayout(null);
 		
-		JButton btnPrestarLibro = new JButton("Prestar Libro");
-		btnPrestarLibro.setBackground(SystemColor.textHighlight);
-		btnPrestarLibro.setBounds(582, 167, 140, 34);
-		getContentPane().add(btnPrestarLibro);
 		
 		
 		
@@ -129,19 +129,52 @@ public class VentanaPrestarLibro extends JInternalFrame {
 		textPrestarA.setBounds(582, 110, 127, 27);
 		getContentPane().add(textPrestarA);
 		
-		JButton btnAgregar = new JButton(">>Agregar");
-		btnAgregar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
+		
+		JButton btnPrestarLibro = new JButton("Prestar Libro");
+		btnPrestarLibro.setEnabled(false);
+		btnPrestarLibro.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			
 				String personaEscogida = listPersona.getSelectedValue();
 				String libroEscogido = listLibro.getSelectedValue();
 				
-				if(!personaEscogida.equals(null) && !libroEscogido.equals(null)){
-				textPrestarA.setText(personaEscogida);
-				textLibroAPrestar.setText(libroEscogido);
-				}
+				System.out.println(personaEscogida);
+				System.out.println(libroEscogido);
+				
+				int iDLibro = biblio.getIDLibro(libroEscogido);
+				
+				System.out.println(iDLibro);
+				
+				
+				JOptionPane.showMessageDialog(null, "Libro Prestado Correctamente");
+				textPrestarA.setText("");
+				textLibroAPrestar.setText("");
 			}
 		});
-		btnAgregar.setBounds(468, 274, 88, 34);
+		btnPrestarLibro.setForeground(SystemColor.textHighlight);
+		btnPrestarLibro.setBackground(SystemColor.activeCaptionBorder);
+		btnPrestarLibro.setBounds(582, 167, 140, 34);
+		getContentPane().add(btnPrestarLibro);
+		
+		
+		JButton btnAgregar = new JButton(">>Agregar");
+		btnAgregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				btnPrestarLibro.setEnabled(true);
+				String personaEscogida = listPersona.getSelectedValue();
+				String libroEscogido = listLibro.getSelectedValue();
+				
+				if(personaEscogida.equals(null) && libroEscogido.equals(null)){
+				JOptionPane.showMessageDialog(null, "Seleccione el libro y la persona a quien desea agregar primero");
+				}
+				textPrestarA.setText(personaEscogida);
+				textLibroAPrestar.setText(libroEscogido);
+				
+				
+				
+			}
+		});
+		btnAgregar.setBounds(468, 274, 102, 34);
 		getContentPane().add(btnAgregar);
 		
 		JLabel lblPersonas = DefaultComponentFactory.getInstance().createLabel("Personas");
---
+++ src/logicaDeNegocios/EnviarMail.java
@@ -0,0 +1,57 @@
+//http://www.chuidiang.com/java/herramientas/javamail/EnviarMail.java.html Referenciade donde descargue el codigo.
+//Librerias necesarias para la correcta funcion : http://www.oracle.com/technetwork/java/javasebusiness/downloads/java-archive-downloads-eeplat-419426.html#javamail-1.4.7-oth-JPR
+
+package logicaDeNegocios;
+
+import logicaDeNegocios.*;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class EnviarMail
+{
+    public static void main(String[] args)
+    {
+        try
+       { 
+            // Propiedades de la conexión
+            Properties props = new Properties();
+            props.setProperty("mail.smtp.host", "smtp.gmail.com");
+            props.setProperty("mail.smtp.starttls.enable", "true");
+            props.setProperty("mail.smtp.port", "587");
+            props.setProperty("mail.smtp.user", "chuidiang@gmail.com");
+            props.setProperty("mail.smtp.auth", "true");
+
+            // Preparamos la sesion
+            Session session = Session.getDefaultInstance(props);
+
+            // Construimos el mensaje
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("alejandrinabiblioteca172@gmail.com"));
+            message.addRecipient(
+                Message.RecipientType.TO,
+                new InternetAddress("arielmmonestel@gmail.com")); //Sitio de destino del msj
+            Biblioteca biblio2= new Biblioteca();
+            message.setSubject("Recordatorio de la devolucion del libro"); //"Subject o motivo del correo"
+            message.setText(
+            		"Hola! se le recuerda que debe devolverme el libro (ID) " + biblio2.getIDLibro("Santa Biblia" )+ " pues ya pasaron los dias aceptables de prestamo \n Pase un buen dia!"); //Correo
+
+            // Lo enviamos.
+            Transport t = session.getTransport("smtp");
+            t.connect("alejandrinabiblioteca172@gmail.com", "alejandrina123"); //Aqui va el correo de donde se va a enviar y la contrasena
+            t.sendMessage(message, message.getAllRecipients());
+
+            // Cierre.
+            t.close();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
